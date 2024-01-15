71ccd7848cb9016bde105002c2590f1a38ce6d31
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-15 12:41:31 -0800

    MySQL_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- A script that prepares a MySQL server for the Project
+-- Create database if it does not exist.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all, and SELECT  privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON perfomance_schema.* TO  'hbnb_test'@'localhost';
+
+-- Flush privileges to apply challenges
+FLUSH PRIVILEGES;
