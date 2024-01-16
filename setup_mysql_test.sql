becedfa3f12b894211d14975b49dd9a3ebe3f10b
TatendaTorerwa <ttorerwa01@gmail.com>
2024-01-16 02:13:12 -0800

    4. MySQL setup test.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,7 +8,7 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all, and SELECT  privileges on hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
-GRANT SELECT ON perfomance_schema.* TO  'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO  'hbnb_test'@'localhost';
 
 -- Flush privileges to apply challenges
 FLUSH PRIVILEGES;
