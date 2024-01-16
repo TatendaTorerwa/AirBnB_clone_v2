f5dd057655537e775f46b5c8abff88105e9d3f17
TatendaTorerwa <ttorerwa01@gmail.com>
2024-01-16 02:09:32 -0800

    3. MySQL setup development.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,7 +10,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
 -- GRANT SELECT privilege on perfomance_schema to hbnb_dev
-GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
