ab7aad68728420420ff8f22be12f200035d6f1da
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-17 07:31:58 -0800

    state.py.

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import os
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
 
 
