d1a36e63d654c9b490c8a8e3f5e656d7617d01b0
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-18 00:40:30 -0800

    4. Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,20 +5,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Route to display Hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbhn():
     """Route to display HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Route to display C followed by the value of the text variable"""
     return "C " + text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -28,6 +32,7 @@ def python_text(text):
     """
     return "Python " + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     """display n is a number only if n is an integer"""
@@ -36,4 +41,3 @@ def number_n(n):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
