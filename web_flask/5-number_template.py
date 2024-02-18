6a8011b8b1e0d8da45a171274da757b0d2525637
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-18 00:43:50 -0800

    5. Number template.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,20 +5,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Route to display Hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbhn():
     """Route to display HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Route to display C followed by the value of the text variable"""
     return "C " + text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -28,10 +32,12 @@ def python_text(text):
     """
     return "Python " + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     """display n is a number only if n is an integer"""
     return f"{n} is a number"
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
@@ -40,6 +46,6 @@ def number_template(n):
     """
     return render_template('5-number.html', n=n)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
